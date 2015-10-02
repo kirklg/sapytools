5450568f1399a5a1cd09d9d024065ad5e24fae55
Kirk Leon Guerrero <kleonguerrero@x1>
2015-10-02 00:35:50 -0700

    New openvz utilities, beancounter interface to provide better error handling/reporting for python code running in an OpenVZ environment. Also for managing/monitoring OpenVZ instances

---
+++ openvz/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'kirk.leonguerrero@gmail.com'
+__version__ = '1.0'
---
+++ openvz/beancounter.py
@@ -0,0 +1,93 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 Kirk Leon Guerrero
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+
+Needs root :(
+Set and Get functions to provide better error handling/reporting to
+applications executing in an OpenVZ environment.
+
+"""
+
+import subprocess
+from collections import namedtuple
+
+class BeancounterNotFound(Exception):
+    """ raise when counter is invalid """
+    pass
+
+class BeanCounter(object):
+    """ openvz.BeanCounter interface """
+    def __init__(self):
+        self.beancounter = '/proc/user_beancounters'
+        self.ubc = dict()
+        self.limits = ('held', 'maxheld', 'barrier', 'limit', 'failcnt')
+        self.counters = (
+            'kmemsize', 'lockedpages', 'privvmpages', 'shmpages', 'numproc',
+            'physpages', 'vmguarpages', 'oomguarpages', 'numtcpsock', 'numflock',
+            'numpty', 'numsiginfo', 'tcpsndbuf', 'tcprcvbuf', 'othersockbuf',
+            'dgramrcvbuf', 'numothersock', 'dcachesize', 'numfile', 'numiptent',)
+        self.counter_base = {fld: namedtuple(fld, list(self.limits)) for fld in self.counters}
+
+        with open(self.beancounter, 'r') as bc_fd:
+            for line in bc_fd:
+                line = line.split()
+                if line[0] not in ('Version:', 'uid'):
+                    # veid identifier
+                    if ':' in line[0]:
+                        veid = int(line[0].replace(':', ''))
+                        counter = line[1]
+                        self.ubc.setdefault(veid, dict())
+                        if counter in self.counters:
+                            self.ubc[veid][counter] = self.counter_base[counter]._make(line[2:])
+                    else:
+                        counter = line[0]
+                        if counter in self.counters:
+                            self.ubc[veid][counter] = self.counter_base[counter]._make(line[1:])
+
+    def set(self, veid, counter, min_limit, max_limit):
+        """ Run vzctl set command and save beancounter for veid """
+        try:
+            from sh import vzctl
+        except ImportError:
+            raise
+        set_cmd = 'vzctl set {veid} --{counter}={min_limit}:{max_limit} --save'.format(**locals())
+        if counter not in self.counters:
+            raise BeancounterNotFound(counter)
+        else:
+            try:
+                subprocess.check_call(set_cmd, shell=True)
+            except subprocess.CalledProcessError:
+                raise
+
+    def get(self, counter=None, veid=None, _all=False):
+        """ Return beancounters """
+        if veid and not counter:
+            return self.ubc[veid]
+        elif veid and counter:
+            try:
+                return self.ubc[veid][counter]
+            except KeyError:
+                BeancounterNotFound(counter)
+
+        if _all:
+            return self.ubc
