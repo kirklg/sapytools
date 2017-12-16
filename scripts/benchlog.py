e0f05c85fd2e7bf1f675651d94f7156f470cca09
Kirk Leon Guerrero <kirk@domaintools.com>
2017-12-15 21:42:39 -0800

    Strip LICENSE from benchlog and add docstring

--- scripts/benchlog.py
+++ scripts/benchlog.py
@@ -1,27 +1,19 @@
 """
-The MIT License (MIT)
+Log event counter, every line (event) appended to a log file will increment the
+global counter, and display the current count per sleep count.
 
-Copyright (c) 2015 Kirk Leon Guerrero
+Where this is useful:
+* For very active log files where `tail -F` is futile.
+* When you have a service-specific event log and you don't care
+about what the data looks like but want to know how active the log is.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Example:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
+# Count events every second
+python ./benchlog.py -f /var/log/syslog -s 1
 """
+
+
 from __future__ import absolute_import, division, print_function
 
 import argparse
